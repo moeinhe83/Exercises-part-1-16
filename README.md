dbc481c78bc77efe3bde8622da6b8ab25d171d17
Moein Heshmati <moeinheshmati1383@gmail.com>
2024-06-24 11:02:16 +0330

    Upload Exercises-part-1-16 Project On Github

---
+++ README.md
@@ -0,0 +1,27 @@
+Project Name ===> Exercises Part 1
+
+Question Number ===> 16
+
+Programming Language ===> Python
+
+Program description ===> 16th Exercise
+
+Example ===> Write a Program That Deletes The Name key From the Above Dictionary
+
+A => info = {'name’:'moein', 'age':20}
+
+Description Of Files:
+
+practice_16_linux.py ===> For Linux 
+
+For Run ===> python3 practice_16_linux.py
+
+practice_16_windows.py ===> For Windows
+
+For Run ===> python practice_16_windows.py
+
+Required packages For Python ===> os / termcolor2 / pyfiglet
+
+termcolor2 ===> To install => pip install termcolor2
+
+pyfiglet ===> To install => pip install pyfiglet
---
+++ practice_16_linux.py
@@ -0,0 +1,26 @@
+# Practice_16
+
+# Library
+from os import system as sys
+from termcolor2 import colored
+from pyfiglet import figlet_format
+
+# Clear Terminal
+sys('clear')
+
+# Intro
+print(colored(figlet_format("Exercises Part 1"), color='cyan'))
+print(colored('=====================================================================', color='red'))
+print(colored('Question Number ===> 16', color='blue'))
+print(colored('// Be Sure To Read The README File To See The Question //', color='blue'))
+print('                                            ')
+
+# Value
+info = {'name':'moein', 'age':20}
+# Remove Name 
+del info['name']
+# Print Dictionary
+print(info)
+
+# Finish
+# Create By Moein Heshmati
